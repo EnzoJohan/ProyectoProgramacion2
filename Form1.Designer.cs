b40e95750a8d97024f4e35205d8fbc637e1c8e3e
EnzoJohan <143907383+EnzoJohan@users.noreply.github.com>
2024-09-13 15:46:10 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ProyectoProgramacion1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCombo = new System.Windows.Forms.Label();
+            this.lblPrecioBase = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.cmbCombos = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtPrecioBase = new System.Windows.Forms.TextBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCombo
+            // 
+            this.lblCombo.AutoSize = true;
+            this.lblCombo.Location = new System.Drawing.Point(355, 54);
+            this.lblCombo.Name = "lblCombo";
+            this.lblCombo.Size = new System.Drawing.Size(40, 13);
+            this.lblCombo.TabIndex = 0;
+            this.lblCombo.Text = "Combo";
+            // 
+            // lblPrecioBase
+            // 
+            this.lblPrecioBase.AutoSize = true;
+            this.lblPrecioBase.Location = new System.Drawing.Point(353, 96);
+            this.lblPrecioBase.Name = "lblPrecioBase";
+            this.lblPrecioBase.Size = new System.Drawing.Size(61, 13);
+            this.lblPrecioBase.TabIndex = 1;
+            this.lblPrecioBase.Text = "PrecioBase";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Location = new System.Drawing.Point(355, 147);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(59, 13);
+            this.lblDescuento.TabIndex = 2;
+            this.lblDescuento.Text = "Descuento";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(12, 343);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total";
+            // 
+            // cmbCombos
+            // 
+            this.cmbCombos.FormattingEnabled = true;
+            this.cmbCombos.Items.AddRange(new object[] {
+            "simple",
+            "Doble",
+            "Premium"});
+            this.cmbCombos.Location = new System.Drawing.Point(423, 51);
+            this.cmbCombos.Name = "cmbCombos";
+            this.cmbCombos.Size = new System.Drawing.Size(121, 21);
+            this.cmbCombos.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(3, 11);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(154, 98);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtPrecioBase
+            // 
+            this.txtPrecioBase.Location = new System.Drawing.Point(420, 96);
+            this.txtPrecioBase.Name = "txtPrecioBase";
+            this.txtPrecioBase.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecioBase.TabIndex = 6;
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(423, 144);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.Size = new System.Drawing.Size(100, 20);
+            this.txtDescuento.TabIndex = 8;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(68, 343);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(526, 358);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(123, 90);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(680, 395);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(108, 53);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDescuento);
+            this.Controls.Add(this.txtPrecioBase);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cmbCombos);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDescuento);
+            this.Controls.Add(this.lblPrecioBase);
+            this.Controls.Add(this.lblCombo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCombo;
+        private System.Windows.Forms.Label lblPrecioBase;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ComboBox cmbCombos;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtPrecioBase;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoProgramacion1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double precioBase = 0;
+            double descuento = 0;
+            double precioFinal = 0;
+
+            switch (cmbCombos.SelectedIndex)
+            {
+                case 0: // Combo Simple
+                    precioBase = 5.0;
+                    descuento = 0.05;
+                    break;
+                case 1: // Combo Doble
+                    precioBase = 10.0;
+                    descuento = 0.10;
+                    break;
+                case 2: // Combo Premium
+                    precioBase = 20.0;
+                    descuento = 0.15;
+                    break;
+                default:
+                    MessageBox.Show("Seleccione un combo válido.");
+                    return;
+            }
+
+            precioFinal = precioBase - (precioBase * descuento);
+
+            txtPrecioBase.Text = "$" + precioBase.ToString();
+            txtDescuento.Text = (descuento * 100).ToString() + "%";
+            txtTotal.Text = "$" + precioFinal.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            cmbCombos.SelectedIndex = -1; // Ninguna opción seleccionada
+
+            txtPrecioBase.Clear();
+            txtDescuento.Clear();
+            txtTotal.Clear();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
